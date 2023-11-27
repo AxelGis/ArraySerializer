49259eab724a7c6d3825ca9b97ab1a169fa056b8
AxelGis <axelgis74@gmail.com>
2023-11-27 10:17:22 +0300

    build js

--- index.js
+++ index.js
@@ -1,64 +1,97 @@
 "use strict";
 const ASCII_START = 34; //skip empty chars
-const ASCII_END = 65535; //max ascii char
+const ASCII_END = 65535; //last ascii char
+/**
+ * Serialize array
+ * @param arr
+ * @returns
+ */
 const serialize = (arr) => {
-    if (arr.length > 100) {
+    if (arr.length > 1000) {
         throw new Error('Max 1000 numbers!');
     }
     arr.sort((a, b) => a - b);
     const compressed = [];
-    let sequense = '';
+    let split = '';
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < 1 || arr[i] > 300) {
-            throw new Error('Invalid number!');
+        const currentNum = arr[i].toString();
+        if (split === '') {
+            split = currentNum;
         }
-        if (Number(sequense + arr[i]) <= ASCII_END) {
-            sequense += arr[i];
+        else if (+(split + '000' + currentNum) <= ASCII_END) {
+            split += '000' + currentNum;
         }
         else {
-            compressed.push(+sequense);
-            sequense = '';
+            compressed.push(+split);
+            split = currentNum;
         }
     }
-    if (sequense != '') {
-        compressed.push(+sequense);
+    if (split !== '') {
+        compressed.push(+split);
     }
     const serializedArr = compressed.map((num) => String.fromCharCode(num + ASCII_START)).join('');
     return serializedArr;
 };
+/**
+ * Deserialize string
+ * @param str
+ * @returns
+ */
 const deserialize = (str) => {
-    const deserializedArr = str.split('').map((char) => char.charCodeAt(0) - ASCII_START);
-    return deserializedArr;
+    const deserializedArr = str.split('');
+    let result = [];
+    for (const char of deserializedArr) {
+        const decoded = char.charCodeAt(0) - ASCII_START;
+        const decodedStr = decoded.toString();
+        if (decodedStr.includes('000')) {
+            result.push(...decoded.toString().split('000'));
+        }
+        else {
+            result.push(decodedStr);
+        }
+    }
+    return result.map((ch) => +ch);
 };
+/**
+ * Get random number between min and max
+ * @param min
+ * @param max
+ * @returns
+ */
 const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+/**
+ * Generate tests
+ * @param condition
+ * @returns
+ */
 const generateTests = (condition) => {
     const array = [];
     switch (condition) {
         case "random50":
             for (let i = 0; i < 50; i++) {
-                array.push(getRandom(0, 300));
+                array.push(getRandom(1, 300));
             }
             break;
         case "random100":
             for (let i = 0; i < 100; i++) {
-                array.push(getRandom(0, 300));
+                array.push(getRandom(1, 300));
             }
             break;
         case "random500":
             for (let i = 0; i < 500; i++) {
-                array.push(getRandom(0, 300));
+                array.push(getRandom(1, 300));
             }
             break;
         case "random1000":
             for (let i = 0; i < 1000; i++) {
-                array.push(getRandom(0, 300));
+                array.push(getRandom(1, 300));
             }
             break;
         case "1sign":
-            for (let i = 0; i < 10; i++) {
-                array.push(i);
+            for (let i = 1; i < 100; i++) {
+                array.push(getRandom(1, 9));
             }
             break;
         case "2sign":
@@ -72,7 +105,7 @@ const generateTests = (condition) => {
             }
             break;
         case "repeat3":
-            for (let i = 0; i < 301; i++) {
+            for (let i = 1; i < 301; i++) {
                 array.push(i, i, i);
             }
             break;
@@ -82,19 +115,16 @@ const generateTests = (condition) => {
 const main = () => {
     const tests = ['random50', 'random100', 'random500', 'random1000', '1sign', '2sign', '3sign', 'repeat3'];
     tests.map((test) => {
-        console.log(test);
-        const arr = generateTests('random50').sort((a, b) => a - b);
+        console.log(`\nstarting test: ${test}`);
+        const arr = generateTests(test).sort((a, b) => a - b);
         const serialized = serialize(arr);
         const deserialized = deserialize(serialized);
         const simpleSerialized = arr.join('');
         if (simpleSerialized !== deserialized.join('')) {
-            console.log(arr);
-            console.log(deserialized);
             throw new Error('Deserialize error');
         }
-        const compression = (100 * serialized.length / deserialized.length).toFixed(2);
-        console.log(arr);
-        console.log(`serialized: ${serialized}`);
+        const compression = (100 * serialized.length / simpleSerialized.length).toFixed(2);
+        console.log(`original: ${simpleSerialized.length} - serialized: ${serialized.length}`);
         console.log(`compression: ${compression}%`);
     });
 };
